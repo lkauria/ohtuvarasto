c10ba9e8a14b1ca8e8becaad43e53e0d29a9cb56
Laura Kauria <laura.kauria@helsinki.fi>
2022-11-16 21:31:14 +0200

    test pylint fail in build

--- src/varasto.py
+++ src/varasto.py
@@ -36,5 +36,6 @@ class Varasto:
         self.saldo = self.saldo - maara
         return maara
 
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
